8fdcee0f4236428bff614f1cb241d79599d3f91c
developer <developer@hunthome.dev>
2024-02-10 21:53:17 -0600

    Add storage.Locations.LastRefreshDate

--- db_HuntHome/storage/Tables/Locations.sql
+++ db_HuntHome/storage/Tables/Locations.sql
@@ -3,7 +3,8 @@
 	[LocationID] SMALLINT IDENTITY(1,1) NOT NULL,
 	[MachineID] VARCHAR(20) NOT NULL,
 	[RootPath] VARCHAR(50) NOT NULL,
-	CONSTRAINT PK_storageLocations_LocationID PRIMARY KEY (LocationID),
+	[LastRefreshDate] DATETIME NULL, 
+    CONSTRAINT PK_storageLocations_LocationID PRIMARY KEY (LocationID),
 	CONSTRAINT FK_storageLocation_MachineID FOREIGN KEY (MachineID) REFERENCES storage.Machines(MachineID),
 	CONSTRAINT UC_storageLocations_MachineRoots UNIQUE (MachineID, RootPath)
 )
