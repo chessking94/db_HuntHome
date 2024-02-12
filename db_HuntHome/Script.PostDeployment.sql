50095de866baeaafce8e72147ff29c7052a80291
developer <developer@hunthome.dev>
2024-02-11 20:58:46 -0600

    Add dynamic user/permissions process to PostDeployment script

--- db_HuntHome/Script.PostDeployment.sql
+++ db_HuntHome/Script.PostDeployment.sql
@@ -13,6 +13,85 @@ Post-Deployment Script Template
 USE HuntHome
 GO
 
+/* Perform the necessary user/role checks for the pythonuser_dev/pythonuser_prod login */
+DECLARE @server varchar(15) = @@SERVERNAME
+DECLARE @login_name varchar(25)
+DECLARE @xsql nvarchar(500)
+DECLARE @login_ct tinyint
+DECLARE @user_ct tinyint
+DECLARE @role_ct tinyint
+
+--get username for environment and verify it exists on the server
+IF @server LIKE '%dev%'
+BEGIN
+	SET @login_name = 'pythonuser_dev'
+END
+ELSE
+BEGIN
+	SET @login_name = 'pythonuser_prod'
+END
+
+SET @xsql = 'SELECT @loginctOUT = COUNT(name) FROM sys.server_principals WHERE name = ''' + @login_name + ''' AND type_desc = ''SQL_LOGIN'''
+EXEC sp_executesql @xsql, N'@loginctOUT tinyint OUTPUT', @loginctOUT=@login_ct OUTPUT
+IF @login_ct = 0
+BEGIN
+	RAISERROR('The user does not exist', 16, 1)
+	RETURN
+END
+
+--create user on database if it does not already exist
+SET @xsql = 'SELECT @userctOUT = COUNT(name) FROM sys.database_principals WHERE name = ''' + @login_name + ''''
+EXEC sp_executesql @xsql, N'@userctOUT tinyint OUTPUT', @userctOUT=@user_ct OUTPUT
+IF @user_ct = 0
+BEGIN
+	SET @xsql = 'CREATE USER [' + @login_name + '] FOR LOGIN [' + @login_name + '] WITH DEFAULT_SCHEMA=[dbo]'
+	EXEC sp_executesql @xsql
+	PRINT 'User created on database'
+END
+
+--the pythonuser_* login should have db_datareader and db_datawriter roles on the database, check if assigned and assign if not
+SET @xsql = '
+SELECT
+@rolectOUT = COUNT(dp2.name)
+
+FROM sys.database_principals AS dp
+JOIN sys.database_role_members AS drm ON dp.principal_id = drm.member_principal_id
+JOIN sys.database_principals AS dp2 ON drm.role_principal_id = dp2.principal_id
+
+WHERE dp2.name = ''db_datawriter''
+AND dp.name = ''' + @login_name + ''''
+
+EXEC sp_executesql @xsql, N'@rolectOUT tinyint OUTPUT', @rolectOUT=@role_ct OUTPUT
+IF @role_ct = 0
+BEGIN
+	--db_datawriter does not exist, add it
+	SET @xsql = 'ALTER ROLE [db_datawriter] ADD MEMBER [' + @login_name + ']'
+	EXEC sp_executesql @xsql
+	PRINT 'Added db_datawriter role to user'
+END
+
+SET @xsql = '
+SELECT
+@rolectOUT = COUNT(dp2.name)
+
+FROM sys.database_principals AS dp
+JOIN sys.database_role_members AS drm ON dp.principal_id = drm.member_principal_id
+JOIN sys.database_principals AS dp2 ON drm.role_principal_id = dp2.principal_id
+
+WHERE dp2.name = ''db_datareader''
+AND dp.name = ''' + @login_name + ''''
+
+EXEC sp_executesql @xsql, N'@rolectOUT tinyint OUTPUT', @rolectOUT=@role_ct OUTPUT
+IF @role_ct = 0
+BEGIN
+	--db_datareader does not exist, add it
+	SET @xsql = 'ALTER ROLE [db_datareader] ADD MEMBER [' + @login_name + ']'
+	EXEC sp_executesql @xsql
+	PRINT 'Added db_datareader role to user'
+END
+
+
+/* Insert seed data */
 --Schema: sftp
 ----Table: LoginTypes
 SET IDENTITY_INSERT sftp.LoginTypes ON
@@ -102,3 +181,14 @@ SELECT '2', 'Virtual'
 WHERE NOT EXISTS (SELECT MachineTypeID FROM storage.MachineTypes WHERE MachineTypeID = '2')
 
 SET IDENTITY_INSERT storage.MachineTypes OFF
+
+
+----Table: Machines
+INSERT INTO storage.Machines (MachineID, MachineTypeID, HostMachine)
+SELECT 'PCSP-JVBBGB2', 1, NULL
+WHERE NOT EXISTS (SELECT MachineID FROM storage.Machines WHERE MachineID = 'PCSP-JVBBGB2')
+
+INSERT INTO storage.Machines (MachineID, MachineTypeID, HostMachine)
+SELECT 'HUNT-PC1', 1, NULL
+WHERE NOT EXISTS (SELECT MachineID FROM storage.Machines WHERE MachineID = 'HUNT-PC1')
+
