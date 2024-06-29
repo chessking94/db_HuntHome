6101e6665cb1af225b7cfd6045db6fb21f0836b6
Ethan Hunt <chessking94@gmail.com>
2024-06-29 15:51:10 -0500

    Add table to track last ID value processed by monitoring processes

---
+++ db_HuntHome/dbo/Tables/LastProcessed.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[LastProcessed]
+(
+	[Schema_Name] nvarchar(255) NOT NULL,
+	[Table_Name] nvarchar(255) NOT NULL,
+	[Last_ID] int CONSTRAINT [DF_LProc_LastID] DEFAULT 0 NOT NULL,
+	[Update_Date] datetime NULL,
+	CONSTRAINT [PK_LProc] PRIMARY KEY CLUSTERED ([Schema_Name] ASC, [Table_Name] ASC)
+);
