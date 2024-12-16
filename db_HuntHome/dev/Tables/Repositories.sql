207b68bc3781f35356b71c4a96b35db54885d13a
Ethan Hunt <chessking94@gmail.com>
2024-12-15 21:11:49 -0600

    Restructure dev.Repositories

--- db_HuntHome/dev/Tables/Repositories.sql
+++ db_HuntHome/dev/Tables/Repositories.sql
@@ -1,12 +1,11 @@
 ﻿CREATE TABLE [dev].[Repositories]
 (
-	[RepoName] VARCHAR(50) NOT NULL, 
+	[RepoName] VARCHAR(50) NOT NULL,
     [AutomatedDeployment] BIT CONSTRAINT [DF_Repositories_AutomatedDeployment] DEFAULT (0) NOT NULL,
     [DeploymentGroup] TINYINT NOT NULL,
-    [RepoPath] VARCHAR(100) NOT NULL, 
-    [GitBranch] VARCHAR(20) NOT NULL, 
-    [RepoLanguage] VARCHAR(10) NOT NULL, 
-    [PublishPath] VARCHAR(100) NULL, 
+    [ProjectFilePath] VARCHAR(100) NOT NULL,
+    [GitBranch] VARCHAR(20) NOT NULL,
+    [PublishPath] VARCHAR(100) NULL,
     [PostDeployBatchFile] VARCHAR(100) NULL,
     [DeploymentQueued] BIT CONSTRAINT [DF_Repositories_DeploymentQueued] DEFAULT (0) NOT NULL,
     [LastDeployedDate] DATETIME NULL,
